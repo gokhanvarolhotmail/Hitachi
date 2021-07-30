5b83aa7707eb9d076f2547f7b965d5e1aa307c0d
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2021-07-30 15:51:29 -0400

    gv

---
+++ SSMSTemplates/Foresters/Flow.sql
@@ -0,0 +1,16 @@
+RETURN
+-- PL_SQL_ASQL_DacadooID
+TRUNCATE TABLE [dbo].[STG_For_memberprofileExtensionBase_LOAD]
+EXEC [dbo].[spOverwriteDacadooID]
+
+-- PL_SFTP_ADLG2_ACTIVITYCHALLENGE
+	-- PL_ADLG2_ASQLDB_ACTIVITYCHALLENGE_REWARDS
+	TRUNCATE TABLE [dbo].[STG_ActivityChallenge_LOAD]
+	EXEC [dbo].[spOverwriteActivityChallenge]
+	EXEC [dbo].[SP_Fact_RewardsPoints_LOAD]
+
+
+-- PL_DACADOO_REWARDPOINTS_MASTER
+EXEC [dbo].[SP_Fact_RewardPointsExport]
+	-- PL_ASQL_API_DACADOO_REWARDPOINTS
+	EXEC [dbo].[lookup_Fact_RewardPoints_EXPORT]

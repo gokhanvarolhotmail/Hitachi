9d699f8ed8108c2868e438719179eaf187f16354
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2021-08-06 15:21:41 -0400

    gv

--- SSMSTemplates/Foresters/MergeLogic.sql
+++ SSMSTemplates/Foresters/MergeLogic.sql
@@ -3,97 +3,72 @@ AS
     BEGIN TRY
         SET NOCOUNT ON ;
 
-        DECLARE @CurrentDate DATETIME ;
-        DECLARE @LastLoadDate DATETIME ;
-        DECLARE @LoadDate DATETIME ;
-        DECLARE @ReseedKey BIGINT ;
-        DECLARE @InsertedDate DATETIME ;
-        DECLARE @ProcedureName VARCHAR(100) ;
+        DECLARE
+            @CurrentDate   DATETIME     = CONVERT(DATETIME, GETDATE())AT TIME ZONE 'UTC' AT TIME ZONE 'Eastern Standard Time'
+          , @LastLoadDate  DATETIME
+          , @InsertedDate  DATETIME     = CONVERT(DATETIME, GETDATE())AT TIME ZONE 'UTC' AT TIME ZONE 'Eastern Standard Time'
+          , @ProcedureName VARCHAR(100) = 'dbo.spOverwriteActivityChallenge' ;
 
-        SET @ProcedureName = 'dbo.spOverwriteActivityChallenge' ;
-        SET @CurrentDate = CONVERT(DATETIME, GETDATE())AT TIME ZONE 'UTC' AT TIME ZONE 'Eastern Standard Time' ;
         SET @LastLoadDate = ( SELECT MAX([LoadDate])FROM [dbo].[CTRL_TableLoadDate] WHERE [TableName] = 'dbo.STG_ActivityChallenge' ) ;
-        SET @InsertedDate = CONVERT(DATETIME, GETDATE())AT TIME ZONE 'UTC' AT TIME ZONE 'Eastern Standard Time' ;
 
-        BEGIN
-            MERGE [dbo].[STG_ActivityChallenge] AS [target]
-            USING( SELECT
-                       [ContactID]
-                     , [dacadoo_ID]
-                     , [EventID]
-                     , [Dollar Value (USD)]
-                     , [RewardDate]
-                     , [PointsAwarded]
-                     , REPLACE(REPLACE([DescriptionText], CHAR(10), CHAR(32)), CHAR(13), CHAR(32)) AS [DescriptionText]
-                   FROM [dbo].[STG_ActivityChallenge_LOAD]
-                   WHERE [dacadoo_ID] IS NOT NULL AND [EventID] IS NOT NULL AND [ContactID] IS NOT NULL ) AS [source]
-            ON [target].[dacadoo_ID] = [source].[dacadoo_ID]
-           AND [target].[EventID] = [source].[EventID]
-           AND [target].[ContactID] = [source].[ContactID]
-           AND ( /*Condition 1*/ [target].[PointsAwarded] = [source].[PointsAwarded]
-               AND [target].[DescriptionText] = [source].[DescriptionText]
-               OR
-               /*Condition 2*/ [target].[RewardDate] = [source].[RewardDate]
-             AND [target].[DescriptionText] = [source].[DescriptionText]
-             AND [target].[PointsAwarded] <> [source].[PointsAwarded]
-             AND NOT( /*Condition 1*/ [target].[PointsAwarded] = [source].[PointsAwarded] AND [target].[DescriptionText] = [source].[DescriptionText] ))
-            WHEN MATCHED THEN
-                UPDATE SET
-                    [target].[RewardDate] = [source].[RewardDate]
-                  , [target].[PointsAwarded] = CASE WHEN [target].[PointsAwarded] = [source].[PointsAwarded]
-                                                    AND [target].[DescriptionText] = [source].[DescriptionText] THEN [source].[PointsAwarded] ELSE [target].[PointsAwarded] + [source].[PointsAwarded] END
-                  , [target].[Dollar Value (USD)] = [source].[Dollar Value (USD)]
-                  , [target].[DescriptionText] = [source].[DescriptionText]
-                  , [target].[UpdatedDateTime] = @CurrentDate
-                  , [target].[UpdatedDateID] = CONVERT(VARCHAR(8), CAST(@CurrentDate AS DATE), 112)
-            WHEN NOT MATCHED THEN --Null record values do not merge and serve no function in rewarding so they are excluded
-                INSERT( [ContactID]
-                      , [dacadoo_ID]
-                      , [EventID]
-                      , [Dollar Value (USD)]
-                      , [RewardDate]
-                      , [PointsAwarded]
-                      , [DescriptionText]
-                      , [InsertedDateTime]
-                      , [UpdatedDateTime]
-                      , [UpdatedDateID] )
-                VALUES(
-                          [source].[ContactID]
-                        , [source].[dacadoo_ID]
-                        , [source].[EventID]
-                        , [source].[Dollar Value (USD)]
-                        , [source].[RewardDate]
-                        , [source].[PointsAwarded]
-                        , [source].[DescriptionText]
-                        , @CurrentDate
-                        , @CurrentDate
-                        , CONVERT(VARCHAR(8), @CurrentDate, 112)
-                      ) ;
+        MERGE [dbo].[STG_ActivityChallenge] AS [t]
+        USING( SELECT
+                   [ContactID]
+                 , [dacadoo_ID]
+                 , [EventID]
+                 , [Dollar Value (USD)]
+                 , [RewardDate]
+                 , [PointsAwarded]
+                 , REPLACE(REPLACE([DescriptionText], CHAR(10), CHAR(32)), CHAR(13), CHAR(32)) AS [DescriptionText]
+               FROM [dbo].[STG_ActivityChallenge_LOAD] ) AS [s]
+        ON [t].[dacadoo_ID] = [s].[dacadoo_ID]
+       AND [t].[EventID] = [s].[EventID]
+       AND [t].[ContactID] = [s].[ContactID]
+       AND [t].[DescriptionText] = [s].[DescriptionText]
+       AND [t].[RewardDate] = [s].[RewardDate]
+       AND [t].[PointsAwarded] <> [s].[PointsAwarded]
+        WHEN MATCHED THEN UPDATE SET
+                              [t].[PointsAwarded] = [t].[PointsAwarded] + [s].[PointsAwarded]
+                            , [t].[Dollar Value (USD)] = [t].[Dollar Value (USD)] + [s].[Dollar Value (USD)]
+                            , [t].[UpdatedDateTime] = @CurrentDate
+                            , [t].[UpdatedDateID] = CONVERT(VARCHAR(8), CAST(@CurrentDate AS DATE), 112)
+        WHEN NOT MATCHED THEN
+            INSERT( [ContactID]
+                  , [dacadoo_ID]
+                  , [EventID]
+                  , [Dollar Value (USD)]
+                  , [RewardDate]
+                  , [PointsAwarded]
+                  , [DescriptionText]
+                  , [InsertedDateTime]
+                  , [UpdatedDateTime]
+                  , [UpdatedDateID] )
+            VALUES(
+                      [s].[ContactID]
+                    , [s].[dacadoo_ID]
+                    , [s].[EventID]
+                    , [s].[Dollar Value (USD)]
+                    , [s].[RewardDate]
+                    , [s].[PointsAwarded]
+                    , [s].[DescriptionText]
+                    , @CurrentDate
+                    , @CurrentDate
+                    , CONVERT(VARCHAR(8), @CurrentDate, 112)
+                  ) ;
 
-            IF @LastLoadDate IS NULL
-                INSERT INTO [dbo].[CTRL_TableLoadDate]
-                VALUES( 'dbo.STG_ActivityChallenge', @LoadDate ) ;
-            ELSE
-                UPDATE [dbo].[CTRL_TableLoadDate]
-                SET [LoadDate] = @CurrentDate
-                WHERE [TableName] = 'dbo.STG_ActivityChallenge' ;
-        END ;
+        IF @LastLoadDate IS NULL
+            INSERT INTO [dbo].[CTRL_TableLoadDate]
+            VALUES( 'dbo.STG_ActivityChallenge', @CurrentDate ) ;
+        ELSE
+            UPDATE [dbo].[CTRL_TableLoadDate]
+            SET [LoadDate] = @CurrentDate
+            WHERE [TableName] = 'dbo.STG_ActivityChallenge' ;
     END TRY
     BEGIN CATCH
-
-        --CAPTURE AND RAISE ERRORS
-        DECLARE @ErrorNumber INT ;
-        DECLARE @ErrorSeverity INT ;
-        DECLARE @ErrorState INT ;
-        DECLARE @ErrorMessage NVARCHAR(4000) ;
-
-        SET @ErrorNumber = ERROR_NUMBER() ;
-        SET @ErrorSeverity = ERROR_SEVERITY() ;
-        SET @ErrorState = ERROR_STATE() ;
-        SET @ErrorMessage = CONVERT(NVARCHAR(4000), @ProcedureName + ':: ' + ERROR_MESSAGE()) ;
-
         INSERT INTO [dbo].[CTRL_ErrorLog]
-        VALUES( @CurrentDate, @ProcedureName, @ErrorNumber, @ErrorSeverity, @ErrorState, @ErrorMessage ) ;
+        VALUES( @CurrentDate, @ProcedureName, ERROR_NUMBER(), ERROR_SEVERITY(), ERROR_STATE(), CONCAT(@ProcedureName, ':: ', ERROR_MESSAGE())) ;
+        ;
 
-        RAISERROR(@ProcedureName, @ErrorSeverity, @ErrorState) ;
+        THROW ;
     END CATCH ;
+GO

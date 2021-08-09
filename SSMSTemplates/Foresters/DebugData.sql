919f008065ae473ff3b68bf5bba64fed0fb3d7b6
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2021-08-09 11:00:16 -0400

    gv

--- SSMSTemplates/Foresters/DebugData.sql
+++ SSMSTemplates/Foresters/DebugData.sql
@@ -63,9 +63,6 @@ WHERE [k].[Row] = 1 ;
   SELECT 'select * from ' + t.name + ' WHERE for_memberprofile_id = ''2F6647D3-90D1-EA11-8E3B-005056824D17'''FROM sys.tables t INNER JOIN sys.columns c ON t.object_id = c.object_id AND c.name = 'for_memberprofile_id'
 
 
- select * from Fact_RewardPoints_20210624 WHERE dacadoo_id = '6052f80e4cab0612e60442fa'
-select * from STG_ActivityChallenge_20210715 WHERE dacadoo_id = '6052f80e4cab0612e60442fa'
-select * from STG_ActivityChallenge_LOAD_20210715 WHERE dacadoo_id = '6052f80e4cab0612e60442fa'
 select * from STG_ActivityChallenge WHERE dacadoo_id = '6052f80e4cab0612e60442fa'
 select * from STG_ActivityChallenge_LOAD WHERE dacadoo_id = '6052f80e4cab0612e60442fa'
 select * from Fact_RewardPoints WHERE dacadoo_id = '6052f80e4cab0612e60442fa'

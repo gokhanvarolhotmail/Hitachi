172135974de7a8ed6c70c293a822aebdf24be231
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2021-08-24 15:19:05 -0400

    GV

--- SSMSTemplates/Metadata/ObjectDefinition.sql
+++ SSMSTemplates/Metadata/ObjectDefinition.sql
@@ -29,8 +29,11 @@ GO
 
 DECLARE @Val NVARCHAR(MAX) = N'[SchemaName], [ObjectName], [FQN], [type], [type_desc], [create_date], [modify_date], [definition]' ;
 
-SELECT CONCAT('SELECT
+SELECT CONCAT('DROP TABLE IF EXISTS [#Definition]
+GO
+SELECT
 ', @Val, '
+INTO [#Definition]
 FROM(VALUES') AS [SQL]
 UNION ALL
 SELECT
@@ -38,10 +41,18 @@ SELECT
         CAST('(' AS NVARCHAR(MAX)), '''', REPLACE([s].[name], '''', ''''''), ''', ''', REPLACE([o].[name], '''', ''''''), ''', '''
       , REPLACE(QUOTENAME([s].[name]) + '.' + QUOTENAME([o].[name]), '''', ''''''), ''', ''', TRIM([o].[type])COLLATE SQL_Latin1_General_CP1_CI_AS, ''', '''
       , [o].[type_desc] COLLATE SQL_Latin1_General_CP1_CI_AS, ''', ''', CONVERT(VARCHAR(30), [o].[create_date], 121), ''', '''
-      , CONVERT(VARCHAR(30), [o].[modify_date], 121), ''', ''', REPLACE([m].[definition], '''', ''''''), '''),') AS [SQL]
+      , CONVERT(VARCHAR(30), [o].[modify_date], 121), ''', '''
+      , ( SELECT SUBSTRING([definition_Replaced], NULLIF([b].[NWL], 0), DATALENGTH([definition_Replaced]) - [b].[NWL] - [b].[NWR] + 2) AS [OutVal]
+          FROM( SELECT
+                    PATINDEX('%[^' + CHAR(32) + CHAR(9) + CHAR(13) + CHAR(10) + ']%', [definition_Replaced]) AS [NWL]
+                  , PATINDEX('%[^' + CHAR(32) + CHAR(9) + CHAR(13) + CHAR(10) + ']%', REVERSE([definition_Replaced])) AS [NWR] ) AS [b] ), '''),') AS [SQL]
 FROM [sys].[objects] AS [o]
 INNER JOIN [sys].[schemas] AS [s] ON [o].[schema_id] = [s].[schema_id]
-INNER JOIN [sys].[all_sql_modules] AS [m] ON [m].[object_id] = [o].[object_id]
+INNER JOIN( SELECT *, REPLACE([m].[definition], '''', '''''') AS [definition_Replaced] FROM [sys].[all_sql_modules] AS [m] ) AS [m] ON [m].[object_id] = [o].[object_id]
 UNION ALL
-SELECT CONCAT(')[d] (', @Val, ')') AS [SQL] ;
+SELECT CONCAT(')[d] (', @Val, ')
+GO
+SELECT
+	',@Val,'
+FROM [#Definition]') AS [SQL] ;
 GO
